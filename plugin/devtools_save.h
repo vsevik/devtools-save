fa2b4c77ac31f7eb866a5c669bc8a8283d1a3ae8
Andrey Kosyakov <caseq@chromium.org>
2012-01-10 17:26:01 +0400

    Added missing file.
    TBR=vsevik

---
+++ plugin/devtools_save.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVTOOLS_SAVE_H_
+#define DEVTOOLS_SAVE_H_
+
+#include "third_party/npapi/npfunctions.h"
+
+class DevToolsSave : public NPObject {
+ public:
+  void Save(const char* filename, const char* content);
+};
+
+#endif // DEVTOOLS_SAVE_H_
